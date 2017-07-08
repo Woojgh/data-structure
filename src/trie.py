decfcabb9e2e79ac729058de66410277a5b2697b
Woojgh <jamessalamonsen@gmail.com>
2017-07-08 14:28:30 -0700

    adding more trie

--- src/trie.py
+++ src/trie.py
@@ -1,27 +1,30 @@
+end = '$'
+
+
 class TrieTree(object):
 
-    def __init__(self, iterable=None):
+    def __init__(self):
         # import pdb; pdb.set_trace()
         """This will sety what wwe will be iterating through."""
         self.size = 0
+        self.letters = 0
         self.roots = []
-        _end = '$'
 
     def make_tree(self, *words):
         root = dict()
         for word in words:
+            self.size + 1
             current_word = root
             for letter in word:
+                self.letters + 1
                 current_word = current_word.setdefault(letter, {})
-            current_word[_end] = _end
+            current_word[end] = end
         return root
 
     def insert(self, word):
         word_root = word[0]
         if word_root not in self.roots:
             self.roots.append(word_root)
-        else:
-            
 
     def search(self, word):
         """."""
@@ -30,9 +33,9 @@ class TrieTree(object):
             if letter in current_word_dict:
                 current_word_dict = current_word_dict[letter]
             else:
-                reutnr False
+                return False
         else:
-            if _end in current_word_dict:
+            if end in current_word_dict:
                 return True
             else:
                 return False
