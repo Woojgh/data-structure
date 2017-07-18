c5bf02bf5ee39d4cd1fd3894b0153335e1e844b8
Woojgh <jamessalamonsen@gmail.com>
2017-07-17 20:32:47 -0700

    got radix sort running smoothly

---
+++ src/radix_sort.py
@@ -0,0 +1,15 @@
+from itertools import chain
+
+
+def radix(set, curr_idx=0):
+    bucket = [[] for _ in range(10)]
+    place = curr_idx
+    for number in set:
+        try:
+            value = int(str(number)[:: - 1][place])
+            bucket[value].append(number)
+        except:
+            bucket[0].append(number)
+    if len(bucket[0]) == len(set):
+        return bucket[0]
+    return radix(list(chain.from_iterable(bucket)), place+1)
