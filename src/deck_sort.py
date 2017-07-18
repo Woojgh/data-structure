a3032e6e18bc1da14a5e9fc3775749d62114136a
Woojgh <jamessalamonsen@gmail.com>
2017-07-18 13:17:27 -0700

    finished with deck sort

--- src/deck_sort.py
+++ src/deck_sort.py
@@ -1,4 +1,3 @@
-from insertion_sort import insertion_sort
 """This will sort a deck of cards"""
 """['3', '9', 'A', '5', 'T', '8', '2', '4', 'Q', '7', 'J', '6', 'K']"""
 """['A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K']"""
@@ -7,22 +6,35 @@ from insertion_sort import insertion_sort
 def deck_sort(deck):
     """This will sort a random deck of cards"""
     sorted_deck = []
-    letters = ['A', 'T', 'J', 'Q', 'K']
-    while len(sorted_deck) < len(deck):
-        idx = 0
-        for card in deck:
-            if card in letters:
-                
-            else:
-                try:
-                    if sorted_deck[idx] >= card:
-                        sorted_deck.insert(idx, card)
-                        idx = 0
-                        break
-                    else:
-                        idx += 1
-                except IndexError:
-                    sorted_deck.append(card)
+    idx = 0
+    for card in deck:
+        while True:
+            try:
+                sorted = get_value(sorted_deck[idx])
+                tmp = get_value(card)
+                if sorted >= tmp:
+                    sorted_deck.insert(idx, card)
                     idx = 0
                     break
+                else:
+                    idx += 1
+            except IndexError:
+                sorted_deck.append(card)
+                idx = 0
+                break
     return sorted_deck
+
+
+def get_value(card):
+    if card == 'A':
+        return 14
+    elif card == 'T':
+        return 10
+    elif card == 'J':
+        return 11
+    elif card == 'Q':
+        return 12
+    elif card == 'K':
+        return 13
+    else:
+        return int(card)
