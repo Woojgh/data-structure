97fc21df0928f70584f8571e9413d022b5c6daab
James Salamonsen <jamessalamonsen@gmail.com>
2017-07-30 18:45:18 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name='data-structures',
     desctription='A variety of data structures built with Python.',
     version='0.1',
-    author='James Salamonsen',
+    author='James Salamonsen' 'Alex Short,
     author_email='jamessalamonsen@gmail.com',
     license='MIT',
     py_modules=[],
