e05562dbf8a7e5a32f9aa5bb7ed6628d57f72e20
Woojgh <jamessalamonsen@gmail.com>
2019-04-08 10:03:42 -0700

    Late addition to my data structures.

--- src/insertion_sort.py
+++ src/insertion_sort.py
@@ -1,6 +1,17 @@
 """An example of the insertion sort."""
 
 
+def `(inp):
+    """Performance improved insertion sort."""
+    for i in range(1, len(inp)):
+        j = i
+        saved = inp[i]
+        while(j > 0 and inp[j-1] > saved):
+            j -= 1
+        inp.insert(j, inp.pop(i))
+    return inp
+
+
 def insertion_sort(numbers):
     """Will sort any given number iterable into a min first list."""
     new = []
--- src/quick_sort.py
+++ src/quick_sort.py
@@ -1,23 +1,39 @@
-"""This is an implemntation of quick_sort."""
+"""This is an implementation of quick_sort."""
 
 
 def quick_sort(numbers):
     pivot = numbers[0]
-    pivot_index = numbers.index(pivot)
     store_index = numbers.index(pivot) + 1
     for i in numbers[1:]:
         if i < pivot:
             new_nums = [pivot]
-            bigger_nums = []
             check_list = list(numbers[store_index:])
             count = 0
             for number in check_list:
                 count += 1
                 thing = check_list[count]
-                print(number)
-                print(thing)
+                print(number, thing)
                 if number < pivot:
-                    number, check_list[store_index] = check_list[store_index], number
+                    number, check_list[store_index] = check_list[store_index],
+                    number
                     store_index += 1
-                print(new_nums)
-                print(check_list)
+                print(new_nums, check_list)
+    return new_nums
+
+
+def partition(arr, low, high):
+    i = (low-1)
+    pivot = arr[high]
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i = i+1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+    return (i+1)
+
+
+def quick_sort_of(arr, low, high):
+    if low < high:
+        pi = partition(arr, low, high)
+        quick_sort_of(arr, low, pi-1)
+        quick_sort_of(arr, pi+1, high)
